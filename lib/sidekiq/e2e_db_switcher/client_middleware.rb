bd8686e343e9abbf3c14f61b94c423a581b3d600
acfohegi <125503650+acfohegi@users.noreply.github.com>
2025-04-18 23:51:01 +0400

    fix sidekiq client mw

--- lib/sidekiq/e2e_db_switcher/client_middleware.rb
+++ lib/sidekiq/e2e_db_switcher/client_middleware.rb
@@ -2,7 +2,7 @@ module Sidekiq
   module E2eDbSwitcher
     class ClientMiddleware
       def call(worker_class, job, queue, redis_pool)
-        current_db = ::E2eDbSwitcher::DbSwitcher.current
+        current_db = Sidekiq.server? ? DbSwitcher.db_from_redis : DbSwitcher.current
         job['database_name'] = current_db
         Sidekiq.logger.info("Tagged #{worker_class} with database #{current_db}")
         yield
